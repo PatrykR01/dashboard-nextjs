302de276aad7c152d869e8fc161e60fe00e5fc06
Patrick <patrykromanski11@gmail.com>
2023-08-02 14:56:34 +0200

    Data adding

---
+++ Data/Data.js
@@ -0,0 +1,112 @@
+export const data = [
+    {
+    id: 1,
+    name: {
+      first: 'Jan',
+      last: 'Nowak',
+    },
+    total: 1550.50,
+    status: 'On Hold',
+    method: 'PayPal',
+    date: '15 Minutes ago',
+  },
+  {
+    id: 2,
+    name: {
+      first: 'Krystian',
+      last: 'Frąc',
+    },
+    total: 2549.53,
+    status: 'Processing',
+    method: 'PayPal',
+    date: '23 Minutes ago',
+  },
+  {
+    id: 3,
+    name: {
+      first: 'Szymon',
+      last: 'Szaniak',
+    },
+    total: 703.44,
+    status: 'Completed',
+    method: 'Visa',
+    date: '1 Hour ago',
+  },
+  {
+    id: 4,
+    name: {
+      first: 'Alicja',
+      last: 'Cholak',
+    },
+    total: 150.45,
+    status: 'Processing',
+    method: 'MasterCard',
+    date: '1 Hour ago',
+  },
+  {
+    id: 5,
+    name: {
+      first: 'Stefan',
+      last: 'Haler',
+    },
+    total: 5000.34,
+    status: 'On Hold',
+    method: 'PayPal',
+    date: '2 Hour ago',
+  },
+  {
+    id: 6,
+    name: {
+      first: 'Laura',
+      last: 'Croft',
+    },
+    total: 1295.75,
+    status: 'Completed',
+    method: 'Check',
+    date: '3 Hour ago',
+  },
+  {
+    id: 7,
+    name: {
+      first: 'Michael',
+      last: 'Smith',
+    },
+    total: 89.95,
+    status: 'Completed',
+    method: 'MasterCard',
+    date: '3 Hour ago',
+  },
+  {
+    id: 8,
+    name: {
+      first: 'Janusz',
+      last: 'Bonowski',
+    },
+    total: 359.45,
+    status: 'Completed',
+    method: 'Visa',
+    date: '7 Hour ago',
+  },
+  {
+    id: 9,
+    name: {
+      first: 'John',
+      last: 'Whiting',
+    },
+    total: 50.34,
+    status: 'Completed',
+    method: 'PayPal',
+    date: '1 Day ago',
+  },
+  {
+    id: 10,
+    name: {
+      first: 'Tomasz',
+      last: 'Werak',
+    },
+    total: 2003.43,
+    status: 'Completed',
+    method: 'MasterCard',
+    date: '1 Day ago',
+  },
+];
